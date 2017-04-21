c653bfbbecd689f24d20e127a27669289a92209f
Silas Agnew <agnewsilas@gmail.com>
2017-04-21 14:15:31 -0400

    Added instructions

--- MainAIVDSketch/MainAIVDSketch.ino
+++ MainAIVDSketch/MainAIVDSketch.ino
@@ -27,7 +27,12 @@
 
 //Start Steering Defines and Global Variables
 const int STEERING_PIN = 9;
+const int STEERING_P = 1.0;
+const int STEERING_I = 0;
+const int STEERING_D = 0;
+
 Servo myServo;
+
 void steering(float degreeIn);
 //End Steering Defines and Global Variables
 
