d5d60f217bc3af1fbf2fbd62a1f5276ce259e003
Silas Agnew <agnewsilas@gmail.com>
2017-05-04 13:36:55 -0400

    Finished test task 5

--- MainAIVDSketch/MainAIVDSketch.ino
+++ MainAIVDSketch/MainAIVDSketch.ino
@@ -49,6 +49,8 @@ const float WHEEL_CIRCUMFERENCE = 10 * PI * 0.0254;
 long motorCount = 0;
 
 float Current;
+
+bool enablePID;
 //End Steering Defines and Global Variables
 
 //Start Drive Defines and Global Variables
@@ -127,6 +129,9 @@ int t2Stage;
 int t4Stage;
 //task 5
 int t5Stage;
+float startDist;
+float startAngle;
+const float REQUIRED_SPACE = 2;
 //task 5
 //task 6
 //task 7
@@ -148,6 +153,7 @@ void setup() {
     myServo.attach(STEERING_PIN);
 	pid.SetMode(AUTOMATIC);
 	pid.SetOutputLimits(-60, 60);
+    enablePID = true;
     //End Steering Setup
     //Start Drive Setup
     Serial1.begin(115200);
@@ -180,14 +186,18 @@ void loop() {
     currentTask = (TASK)ToDecimal(TASK_PIN, 3);
 
     gyro.loop();
-    in = gyro.getOrientation(Gyro::kXAxis);
 
-	if (abs(sp - in) <= 180)
-		pidIn = sp - in;
-	else pidIn = sp - in + 360;
+    if (enablePID) 
+    {
+        in = gyro.getOrientation(Gyro::kXAxis);
+    
+    	if (abs(sp - in) <= 180)
+    		pidIn = sp - in;
+    	else pidIn = sp - in + 360;
 
-    pid.Compute();
-    steering(out);
+        pid.Compute();
+        steering(out);
+    }
 
     if (currentTask == none) {
 		Current = 5;
@@ -365,20 +375,67 @@ void loop() {
     }
     else if (currentTask == five)
     {
+        enablePID = false;
         //start task 5 Code:  Parallel park.
         switch (t5Stage)
         {
-            case 0: // Drive forward looking for a spot large enough
+            case 0: 
+                Current = 5;
+                setSpeed();
                 steering(0);
+
+                if (!!IRDistance(IR_FRONT_RIGHT_PIN))
+                {
+                    startDist = CountDistance(motorCount);
+                    t5Stage++;
+                }
             break;
-            case 1: // Turn 45 and back up; continue if at a-pillar
+            case 1: // measure distance
+                Current = 5;
+                setSpeed();
+                steering(0);
+                if (!IRDistance(IR_FRONT_RIGHT_PIN))
+                {
+                    if (CountDistance(motorCount) >= REQUIRED_SPACE + startDist) 
+                    {
+                        startAngle = gyro.getOrientation(Gyro::kXAxis);
+                        t5Stage++;
+                    }
+                }
+                else t5Stage--;
+            break;
+            case 2:
+                if (!!IRDistance(IR_BACK_RIGHT_PIN))
+                {
+                    Current = 0;
+                    setBrake();
+                }
+            break;
+            case 3:
                 steering(45);
+                Current = -3;
+                setSpeed();
+
+                if (!IRDistance(IR_A_PILLAR_RIGHT_PIN)) // Problems
+                {
+                    t5Stage++;
+                }
             break;
-            case 2: // Turn -45 and continue backing up; may have to see if it hits other car 
+            case 4:
                 steering(-45);
+                Current = -3;
+                setBrake();
+                
+                if (gyro.getOrientation(Gyro::kXAxis) <= startAngle)
+                {
+                    t5Stage++;    
+                }
+                // back right too close
+                // front right too close to car in front              
             break;
-            case 3: // Turn 0 and back up to an appropriate distance
-                steering(0);
+            case 6:
+                Current = 0;
+                setBrake(); 
             break;
         }
         //end task5 Code
